98e5d7eaf8171994a0e23985a84b78446457dab9
Trung Tá <trungta.hust@gmail.com>
2023-10-29 08:40:54 +0700

    thêm sự kiện bật tắt cam/mic

--- VideoCall/js/room__rtc.js
+++ VideoCall/js/room__rtc.js
@@ -32,11 +32,16 @@ let joinRoomInit = async () => {
 }
 
 let joinStream = async () => {
-    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
+
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks({}, {encoderConfig:{
+        width:{min: 640, ideal:1920, max:1920},
+        height:{min:480, ideal:1080, max:1080}
+    }})
 
     let player = '<div class="video__container" id = "user-container-${uid}">                     <div class = "video-player" id = "user-${uid}"></div> </div>'
 
     document.getElementById('streams__container').insertAdjacentHTML('beforeend', player)
+    document.getElementById('user-container-${uid}').addEventListener('click',expandVideoFrame)
 
     localTracks[1].play('user-${uid}')
     await client.publish([localTracks[0],localTracks[1]])
@@ -52,7 +57,13 @@ let handleUserPublished = async (user, mediaType) => {
         player = '<div class="video__container" id = "user-container-${user.uid}"> <div class = "video-player" id = "user-${user.uid}"></div> </div>'
 
     document.getElementById('streams__container').insertAdjacentHTML('beforeend', player)
+    document.getElementById('user-container-${uid}').addEventListener('click',expandVideoFrame)
 
+    }
+
+    if(displayFrame.style.display) {
+        player.style.height = '100px'
+        player.style.width = '100px'
     }
 
     if(mediaType === 'video'){
@@ -68,6 +79,44 @@ let handleUserPublished = async (user, mediaType) => {
 let handleUserLeft = async(user) => {
     delete remoteUsers[user.uid]
     document.getElementById('user-container-${user.uid}').remove()
+
+    if(userIdInDisplayFrame === 'user-container-${user.uid}') {
+        displayFrame.style.display = null
+
+        let videoFrames = document.getElementsByClassName('video__container')
+
+        for (let i = 0; videoFrames.length > i; i++) {
+            videoFrames[i].style.height = '300px'
+            videoFrames[i].style.width = '300px'
+        }
+    }
 }
+
+let toggleCamera = async (e) => {
+    let button = e.currentTarget
+
+    if(localTracks[1].muted) {
+        await localTracks[1].setMuted(false)
+        button.classList.add('active')
+    } else {
+        await localTracks[1].setMuted(true)
+        button.classList.remove('active')
+    }
+}
+
+let toggleMic = async (e) => {
+    let button = e.currentTarget
+
+    if(localTracks[0].muted) {
+        await localTracks[0].setMuted(false)
+        button.classList.add('active')
+    } else {
+        await localTracks[0].setMuted(true)
+        button.classList.remove('active')
+    }
+}
+
+document.getElementById('camera-btn').addEventListener('click', toggleCamera)
+document.getElementById('mic-btn').addEventListener('click', toggleMic)
 
 joinRoomInit()
